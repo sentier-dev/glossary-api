c5c36563eeb5175906607d95d0bb589f54eccdc7
sami-m-g <sami.mg@outlook.com>
2024-05-06 20:17:17 +0300

    tests/unit/test_main.py: added type hint for client

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -2,8 +2,10 @@
 
 from http import HTTPStatus
 
+from fastapi.testclient import TestClient
 
-def test_get_concept_schemes_empty(client) -> None:
+
+def test_get_concept_schemes_empty(client: TestClient) -> None:
     """Test the /schemes endpoint with an empty database."""
     response = client.get("/schemes")
     assert response.json() == {"concept_schemes": []}
