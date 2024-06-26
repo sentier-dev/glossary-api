6a6ffe1f2d08e1270d2defb1c3ddb5dfff095634
sami-m-g <sami.mg@outlook.com>
2024-06-26 14:42:38 +0300

    dds_glossary/settings.py: set default value for SENTRY_DSN

--- dds_glossary/settings.py
+++ dds_glossary/settings.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
 
     API_KEY: SecretStr
     DATABASE_URL: SecretStr
-    SENTRY_DSN: SecretStr
+    SENTRY_DSN: SecretStr = SecretStr("")
 
     HOST_IP: str = "127.0.0.1"
 
